e1256fe34e4a3d53e8556e00d395181820beb6ca
Gott50 <xxGott50xx@gmail.com>
2016-12-12 09:35:02 +0100

    add //TODO

--- src/blatt08/pgdp/Money.java
+++ src/blatt08/pgdp/Money.java
@@ -4,22 +4,25 @@ public class Money {
     private int cent;
 
     public Money() {
-
+        //TODO
     }
 
     public Money(int cent) {
-
+        //TODO
     }
 
     public int getCent() {
+        //TODO
         return -1;
     }
 
     public Money addMoney(Money m) {
+        //TODO
         return null;
     }
 
     public String toString() {
+        //TODO
         return null;
     }
 }
