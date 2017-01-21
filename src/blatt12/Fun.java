9ab4d629ab1a91c8dddcd2dab17aa34dcd75cad3
Gott50 <xxGott50xx@gmail.com>
2017-01-21 11:09:05 +0100

    add MapTest

---
+++ src/blatt12/Fun.java
@@ -0,0 +1,5 @@
+package blatt12;
+
+public interface Fun<T, R> {
+    R apply(T x);
+}
---
+++ src/blatt12/Map.java
@@ -0,0 +1,7 @@
+package blatt12;
+
+public class Map {
+    public static <T, R> void map(Fun<T, R> f, T[] a, R[] b, int n) {
+        //TODO
+    }
+}
