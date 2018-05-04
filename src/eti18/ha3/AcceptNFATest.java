e0a097754522340a5c38a6c2f50c6d73f24d4f11
Gott50 <xxGott50xx@gmail.com>
2018-05-04 21:39:26 +0200

    refactor

--- src/eti18/ha3/AcceptNFATest.java
+++ src/eti18/ha3/AcceptNFATest.java
@@ -20,108 +20,33 @@ public class AcceptNFATest {
     @Parameterized.Parameters(name = "{index}: {1} -> {2}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"eDFA\n" +
-                        "Alphabet: a;b;c\n" +
-                        "States: p;q;r\n" +
-                        "Init: p\n" +
-                        "Final: r\n" +
-                        "Transitions:\n" +
-                        "p;a;p\n" +
-                        "p;b;p\n" +
-                        "p;c;q\n" +
-                        "q;a;q\n" +
-                        "q;b;q\n" +
-                        "q;c;r\n" +
-                        "r;a;r\n" +
-                        "r;b;r\n" +
-                        "r;c;r\n" +
-                        "END", "abababc", false},
-                {"eDFA\n" +
-                        "Alphabet: a;b;c\n" +
-                        "States: p;q;r\n" +
-                        "Init: p\n" +
-                        "Final: r\n" +
-                        "Transitions:\n" +
-                        "p;a;p\n" +
-                        "p;b;p\n" +
-                        "p;c;q\n" +
-                        "q;a;q\n" +
-                        "q;b;q\n" +
-                        "q;c;r\n" +
-                        "r;a;r\n" +
-                        "r;b;r\n" +
-                        "r;c;r\n" +
-                        "END", "cc", true},
-                {"eDFA\n" +
-                        "Alphabet: a;b;c\n" +
-                        "States: p;q;r\n" +
-                        "Init: p\n" +
-                        "Final: r\n" +
-                        "Transitions:\n" +
-                        "p;a;p\n" +
-                        "p;b;p\n" +
-                        "p;c;q\n" +
-                        "q;a;q\n" +
-                        "q;b;q\n" +
-                        "q;c;r\n" +
-                        "r;a;r\n" +
-                        "r;b;r\n" +
-                        "r;c;r\n" +
-                        "END", "ccc", true},
-                {"eDFA\n" +
-                        "Alphabet: a;b;c\n" +
-                        "States: p;q;r\n" +
-                        "Init: p\n" +
-                        "Final: r\n" +
-                        "Transitions:\n" +
-                        "p;a;p\n" +
-                        "p;b;p\n" +
-                        "p;c;q\n" +
-                        "q;a;q\n" +
-                        "q;b;q\n" +
-                        "q;c;r\n" +
-                        "r;a;r\n" +
-                        "r;b;r\n" +
-                        "r;c;r\n" +
-                        "END", "caaabbcabaabc", true},
-                {"eDFA\n" +
-                        "Alphabet: a;b;c\n" +
-                        "States: p;q;r\n" +
-                        "Init: p\n" +
-                        "Final: r\n" +
-                        "Transitions:\n" +
-                        "p;a;p\n" +
-                        "p;b;p\n" +
-                        "p;c;q\n" +
-                        "q;a;q\n" +
-                        "q;b;q\n" +
-                        "q;c;r\n" +
-                        "r;a;r\n" +
-                        "r;b;r\n" +
-                        "r;c;r\n" +
-                        "END", "x", false},
-                {"eDFA\n" +
-                        "Alphabet: a;b;c\n" +
-                        "States: p;q;r\n" +
-                        "Init: p\n" +
-                        "Final: r\n" +
-                        "Transitions:\n" +
-                        "p;a;p\n" +
-                        "p;b;p\n" +
-                        "p;c;q\n" +
-                        "q;a;q\n" +
-                        "q;b;q\n" +
-                        "q;c;r\n" +
-                        "r;a;r\n" +
-                        "r;b;r\n" +
-                        "r;c;r\n" +
-                        "END", "ccx", false},
+                {"abababc", false},
+                {"cc", true},
+                {"ccc", true},
+                {"caaabbcabaabc", true},
+                {"x", false},
+                {"ccx", false},
         });
     }
 
 
-    public AcceptNFATest(String n, String w, boolean expected) {
-        this.n = Parser.parse(new Scanner(n));
+    public AcceptNFATest(String w, boolean expected) {
+        this.n = Parser.parse(new Scanner("EpsilonNFA\n" +
+                "Alphabet: a;b;c\n" +
+                "States: p;q;r\n" +
+                "Init: p\n" +
+                "Final: r\n" +
+                "Transitions:\n" +
+                "p;a;p\n" +
+                "p;b;p\n" +
+                "p;c;q\n" +
+                "q;a;q\n" +
+                "q;b;q\n" +
+                "q;c;r\n" +
+                "r;a;r\n" +
+                "r;b;r\n" +
+                "r;c;r\n" +
+                "END"));
         this.w = w;
         this.expected = expected;
     }
